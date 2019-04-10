fa291f2dd8f69d3c2d831a3b35b6908b56594505
Linbin <i@teenet.me>
2019-04-10 14:52:45 -0400

    new

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/taskBoard.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
 import './css/general.css'
 import App from './App';
 
