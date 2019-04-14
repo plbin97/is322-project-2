b6bdb58808a39fd332a750f2efa86623640af0ba
Besfort Ymaj <by56@njit.edu>
2019-04-14 17:37:36 -0400

    readded css for tasklist, and also got the sort / p2 to work

--- src/taskList/TaskItem.js
+++ src/taskList/TaskItem.js
@@ -3,13 +3,9 @@ import React from 'react';
 const TaskItem = props => {
   return (
     <li className="list-group-item">
-      { props.task.title }
-      <div> {props.task.type}
-
-      </div>
-      <div>
-        {props.task.column}
-      </div>
+      { props.task.title } |
+      <span> Type: {props.task.type} |     </span>
+      <span> Status: {props.task.column}  </span>
       <button type="button"
               onClick={() => props.markDone(props.task)}
               className="btn btn-primary" style={{ float: 'right' }}>
--- src/taskList/TaskList.js
+++ src/taskList/TaskList.js
@@ -4,6 +4,11 @@ import TaskItem from './TaskItem';
 
 class TaskList extends React.Component {
 
+  state = {
+    sort:'',
+    filter:[]
+  };
+
   markDone = (task) => {
     const taskIndex = this.props.tasks.findIndex(t => t.id === task.id);
     let taskList = this.props.tasks;
@@ -11,19 +16,67 @@ class TaskList extends React.Component {
     console.log(this.props);
     // this.props.onUpdateTaskList(taskList);
   }
+  sortList() {
+    let taskList = this.props.tasks;
+
+    switch(this.state.sort){
+      case 'title':
+        taskList.sort((a, b) => (a.title > b.title) ? 1 : -1);
+        break;
+      case 'type':
+        taskList.sort((a,b) => (a.type > b.type) ? 1 : -1);
+        break;
+      case 'column':
+        taskList.sort((a,b) => (a.column > b.column) ? 1 : -1);
+        break;
+    }
+  }
 
   render() {
     const taskItems = this.props.tasks.map(task => {
       return <TaskItem task={task} key={task.id} markDone={this.markDone} />
     });
 
+    let sort = (<div id="container">
+      <div id="left">
+        <h3>Sort</h3>
+        <select>
+          <option value="Default">Pick an Option</option>
+          <option value="Title">Title</option>
+          <option value="Status">Status</option>
+          <option value="Type">Type</option>
+        </select>
+      </div>
+      <div id="center">
+        <h3>Status</h3>
+        <select>
+          <option value="Default">Pick an Option</option>
+          <option value="To do">To do</option>
+          <option value="In Progress">In Progress</option>
+          <option value="Review">Review</option>
+          <option value="Done">Done</option>
+        </select>
+      </div>
+      <div id="right">
+        <h3>Type</h3>
+        <select>
+          <option value="Default">Pick an Option</option>
+          <option value="Task">Task</option>
+          <option value="Feature">Feature</option>
+          <option value="Review">Review</option>
+          <option value="Bug">Bug</option>
+        </select>
+      </div>
+    </div>);
+
     return (
 
-      <ul className="task-list list-group">
-        { taskItems }
-      </ul>
+        <div id="sort"> { sort }
+          <ul className="task-list list-group">
+            { taskItems }
+          </ul>
+        </div>
     )
   }
 }
-
 export default TaskList;
